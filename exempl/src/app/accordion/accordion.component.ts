6d9d6759ce9a40df0a4813b0cd3718cd200ce409
Dima <makushin.dima@gmail.com>
2019-09-19 10:10:39 +0300

    up: accordion

--- exempl/src/app/accordion/accordion.component.ts
+++ exempl/src/app/accordion/accordion.component.ts
@@ -13,7 +13,13 @@ export class AccordionComponent implements OnInit {
   footer_code = {
     footer: "\n  constructor() {\n}\n\n  ngOnInit() {\n }\n\n  Accordion(idx) {\n    if (this.isShown(idx)) {\n      this.show = null;\n    } else {\n    this.show = idx;\n    }\n  }\n  isShown(idx) {\n    return this.show === idx;\n  }\n\n}"
   };
-
+  htmlcode =
+    {
+      code: "\n<mat-card class='card' *ngFor='let itm of data_json; let i = index;'>\n    <p class='card-title' (click)='Accordion('idx'+i)'>\n        {{ itm.title }}\n    </p>\n    <div class='card-text' *ngIf='isShown('idx'+i)'>\n        <ol>\n            <li *ngFor='let itmx of itm.ingredients;'>\n                {{ itmx }}\n            </li>\n        </ol>\n    </div>\n</mat-card>"
+    };
+  Csscode = {
+    code: "\n.cards {\n    margin: 5% 35% 0px 35%;\n    .card {\n        margin-top: 50px;\n    }\n    .card-title {\n        font-family: 'Times New Roman', Times, serif;\n        font-size: 21px;\n        color: #c2c2c2;\n        text-align: center;\n    }\n    .card-text {\n        font-family: 'Times New Roman', Times, serif;\n        font-size: 16px;\n        color: #0ee0e7dc;\n        text-align: left;\n    }\n}"
+  };
   show = null;
 
   data_json = [
@@ -61,11 +67,6 @@ export class AccordionComponent implements OnInit {
       ]
     }
   ];
-
-  htmlcode =
-    {
-      code: "\n<mat-card class='card' *ngFor='let itm of data_json; let i = index;'>\n    <p class='card-title' (click)='Accordion('idx'+i)'>\n        {{ itm.title }}\n    </p>\n    <div class='card-text' *ngIf='isShown('idx'+i)'>\n        <ol>\n            <li *ngFor='let itmx of itm.ingredients;'>\n                {{ itmx }}\n            </li>\n        </ol>\n    </div>\n</mat-card>"
-    };
 
   constructor() {
   }
