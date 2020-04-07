04ef9d20d996594cb6b45a3f0789789bf77cfce2
Daemon <makushin.dima@gmail.com>
2020-04-07 09:29:09 +0300

    up

--- exempl/src/app/tinymce/tinymce.component.ts
+++ exempl/src/app/tinymce/tinymce.component.ts
@@ -9,6 +9,8 @@ import { Component, OnInit } from '@angular/core';
 export class TinymceComponent implements OnInit {
 
 	text_editor : any;
+	example_code: any = `<p><span style="background-color: #169179; color: #f1c40f; font-size: 14pt; font-family: tahoma, arial, helvetica, sans-serif;">textt-font new text<code></code></span></p>
+	<p><strong>example code</strong></p>`;
 
 	constructor() { }
 
@@ -18,7 +20,7 @@ export class TinymceComponent implements OnInit {
 	onGetDataEditor(){
 		console.log('edit: ', this.text_editor);
 	}
-
+v
 
 	onfilePost(){
 		console.log('post file ...');
