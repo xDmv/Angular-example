250bc4945aeaad185cb461512cd122a96d50d344
Daemon <makushin.dima@gmail.com>
2020-07-15 15:43:35 +0300

    up: tinymce editor

--- exempl/src/app/tinymce/tinymce.component.ts
+++ exempl/src/app/tinymce/tinymce.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
 	selector: 'app-tinymce',
@@ -9,16 +9,18 @@ import { Component, OnInit } from '@angular/core';
 export class TinymceComponent implements OnInit {
 
 	text_editor : any;
+	edit_text: any;
 	// example_code: any = `<p><span style="background-color: #169179; color: #f1c40f; font-size: 14pt; font-family: tahoma, arial, helvetica, sans-serif;">textt-font new text<code></code></span></p>
 	// <p><strong>example code</strong></p>`;
 
-	constructor() { }
+	constructor(private sanitizer: DomSanitizer) { }
 
 	ngOnInit() {
 	}
 
 	onGetDataEditor(){
 		console.log('edit: ', this.text_editor);
+		this.edit_text = this.sanitizer.bypassSecurityTrustHtml(this.text_editor);
 	}
 
 
